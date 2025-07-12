cde9ca58dd08d2560a36fef7a70fb9872aa85dc7
xyy1090091952 <1090091952@qq.com>
2025-07-12 19:07:17 +0800

    fix(quiz): 修复错题重练模式标识和导航堆栈问题
    将quizMode从'endless'改为'mistakes'以正确标识错题重练模式
    使用wx.redirectTo替代wx.reLaunch解决导航堆栈问题，允许从结果页返回

--- pages/quiz/quiz.js
+++ pages/quiz/quiz.js
@@ -228,8 +228,8 @@ Page({
       }
       
       // 其他模式：检查是否到达最后一题
-      if ((this.data.quizMode === 'quick' && this.data.currentQuestionIndex >= this.data.totalQuestions - 1) ||
-        (this.data.quizMode === 'course' && this.data.currentQuestionIndex >= this.data.totalQuestions - 1)) {
+      if ((this.data.quizMode === 'quick' || this.data.quizMode === 'course' || this.data.quizMode === 'mistakes') && 
+          this.data.currentQuestionIndex >= this.data.totalQuestions - 1) {
         this.endQuiz();
       } else {
         const nextIndex = this.data.currentQuestionIndex + 1;
@@ -303,7 +303,10 @@ Page({
       url += `&fromMistakes=true`;
     }
 
-    wx.reLaunch({
+    // 使用redirectTo替换reLaunch，以修复导航堆栈问题
+    // wx.reLaunch会关闭所有页面，导致无法返回上一页
+    // wx.redirectTo会关闭当前页面，跳转到新页面，这样就可以从结果页返回到之前的页面
+    wx.redirectTo({
       url: url
     });
   },
