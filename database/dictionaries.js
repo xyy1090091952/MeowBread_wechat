df72da9e7de0bd35f45f1105bba8f63dec0a8809
xyy1090091952 <1090091952@qq.com>
2025-07-12 18:16:20 +0800

    feat(duolingguo): 添加第4课数据并更新词典索引
    更新脚本以自动检测新课程并维护词典索引

--- database/dictionaries.js
+++ database/dictionaries.js
@@ -17,21 +17,21 @@ module.exports = {
                 "everyones_japanese/lesson45.js"
             ],
             "volumes": [
-              { "id": "volume1", "name": "第一册", "description": "大家的日语第一册", "lessons": [31, 32, 33, 34, 35, 36, 37] },
-              { "id": "volume2", "name": "第二册", "description": "大家的日语第二册", "lessons": [38, 44, 45] }
-            ],
+            {"id":"volume1","name":"第一册","description":"大家的日语第一册","lessons":[31,32,33,34,35,36,37]},
+            {"id":"volume2","name":"第二册","description":"大家的日语第二册","lessons":[38,44,45]}
+        ],
             "courses": [
-              { "courseNumber": 31, "courseTitle": "第31课", "lessonFile": "lesson31", "description": "大家的日语第31课" },
-              { "courseNumber": 32, "courseTitle": "第32课", "lessonFile": "lesson32", "description": "大家的日语第32课" },
-              { "courseNumber": 33, "courseTitle": "第33课", "lessonFile": "lesson33", "description": "大家的日语第33课" },
-              { "courseNumber": 34, "courseTitle": "第34课", "lessonFile": "lesson34", "description": "大家的日语第34课" },
-              { "courseNumber": 35, "courseTitle": "第35课", "lessonFile": "lesson35", "description": "大家的日语第35课" },
-              { "courseNumber": 36, "courseTitle": "第36课", "lessonFile": "lesson36", "description": "大家的日语第36课" },
-              { "courseNumber": 37, "courseTitle": "第37课", "lessonFile": "lesson37", "description": "大家的日语第37课" },
-              { "courseNumber": 38, "courseTitle": "第38课", "lessonFile": "lesson38", "description": "大家的日语第38课" },
-              { "courseNumber": 44, "courseTitle": "第44课", "lessonFile": "lesson44", "description": "大家的日语第44课" },
-              { "courseNumber": 45, "courseTitle": "第45课", "lessonFile": "lesson45", "description": "大家的日语第45课" }
-            ]
+            {"courseNumber":31,"courseTitle":"第31课","lessonFile":"lesson31","description":"大家的日语第31课"},
+            {"courseNumber":32,"courseTitle":"第32课","lessonFile":"lesson32","description":"大家的日语第32课"},
+            {"courseNumber":33,"courseTitle":"第33课","lessonFile":"lesson33","description":"大家的日语第33课"},
+            {"courseNumber":34,"courseTitle":"第34课","lessonFile":"lesson34","description":"大家的日语第34课"},
+            {"courseNumber":35,"courseTitle":"第35课","lessonFile":"lesson35","description":"大家的日语第35课"},
+            {"courseNumber":36,"courseTitle":"第36课","lessonFile":"lesson36","description":"大家的日语第36课"},
+            {"courseNumber":37,"courseTitle":"第37课","lessonFile":"lesson37","description":"大家的日语第37课"},
+            {"courseNumber":38,"courseTitle":"第38课","lessonFile":"lesson38","description":"大家的日语第38课"},
+            {"courseNumber":44,"courseTitle":"第44课","lessonFile":"lesson44","description":"大家的日语第44课"},
+            {"courseNumber":45,"courseTitle":"第45课","lessonFile":"lesson45","description":"大家的日语第45课"}
+        ]
         },
         {
             "id": "liangs_class",
@@ -61,32 +61,32 @@ module.exports = {
                 "liangs_class/lesson25.js"
             ],
             "volumes": [
-              { "id": "upper", "name": "初级上", "description": "基础日语课程", "lessons": [5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16] },
-              { "id": "lower", "name": "初级下", "description": "进阶日语课程", "lessons": [17, 18, 19, 20, 21, 22, 23, 24, 25] }
-            ],
+            {"id":"upper","name":"初级上","description":"基础日语课程","lessons":[5,6,7,8,9,10,11,12,13,14,15,16]},
+            {"id":"lower","name":"初级下","description":"进阶日语课程","lessons":[17,18,19,20,21,22,23,24,25]}
+        ],
             "courses": [
-              { "courseNumber": 5, "courseTitle": "初対面", "lessonFile": "lesson5", "description": "初次见面的日语表达" },
-              { "courseNumber": 6, "courseTitle": "Wi-Fiはありますか", "lessonFile": "lesson6", "description": "询问Wi-Fi等基本设施" },
-              { "courseNumber": 7, "courseTitle": "スマホに写真がありますよ", "lessonFile": "lesson7", "description": "谈论手机和照片" },
-              { "courseNumber": 8, "courseTitle": "S サイズでお願いします", "lessonFile": "lesson8", "description": "购物时的尺寸选择" },
-              { "courseNumber": 9, "courseTitle": "誕⽣⽇と性格", "lessonFile": "lesson9", "description": "生日和性格的表达" },
-              { "courseNumber": 10, "courseTitle": "映画の約束", "lessonFile": "lesson10", "description": "电影约会的安排" },
-              { "courseNumber": 11, "courseTitle": "電気屋さんで", "lessonFile": "lesson11", "description": "在电器店购物" },
-              { "courseNumber": 12, "courseTitle": "漫画ファン", "lessonFile": "lesson12", "description": "漫画爱好者的对话" },
-              { "courseNumber": 13, "courseTitle": "忘れ物", "lessonFile": "lesson13", "description": "遗忘物品的处理" },
-              { "courseNumber": 14, "courseTitle": "勉強スケジュール", "lessonFile": "lesson14", "description": "学习计划的制定" },
-              { "courseNumber": 15, "courseTitle": "試験勉強", "lessonFile": "lesson15", "description": "考试复习的准备" },
-              { "courseNumber": 16, "courseTitle": "村上夏樹先生の講演会", "lessonFile": "lesson16", "description": "村上夏树先生的讲演会" },
-              { "courseNumber": 17, "courseTitle": "アンケート", "lessonFile": "lesson17", "description": "问卷调查的填写" },
-              { "courseNumber": 18, "courseTitle": "妹からアドバイスをもらいました", "lessonFile": "lesson18", "description": "从妹妹那里得到建议" },
-              { "courseNumber": 19, "courseTitle": "夏休の過ごし方", "lessonFile": "lesson19", "description": "暑假的度过方式" },
-              { "courseNumber": 20, "courseTitle": "ネットで口コミ情報をチェック", "lessonFile": "lesson20", "description": "网上口碑信息的查看" },
-              { "courseNumber": 21, "courseTitle": "返さなくてもいいんですか", "lessonFile": "lesson21", "description": "不用归还也可以吗" },
-              { "courseNumber": 22, "courseTitle": "チームワークが⼀番です", "lessonFile": "lesson22", "description": "团队合作最重要" },
-              { "courseNumber": 23, "courseTitle": "グループ作業", "lessonFile": "lesson23", "description": "小组作业的进行" },
-              { "courseNumber": 24, "courseTitle": "ランチ", "lessonFile": "lesson24", "description": "午餐相关的日语表达" },
-              { "courseNumber": 25, "courseTitle": "日本の弁当文化", "lessonFile": "lesson25", "description": "日本的便当文化" }
-            ]
+            {"courseNumber":5,"courseTitle":"初対面","lessonFile":"lesson5","description":"初次见面的日语表达"},
+            {"courseNumber":6,"courseTitle":"Wi-Fiはありますか","lessonFile":"lesson6","description":"询问Wi-Fi等基本设施"},
+            {"courseNumber":7,"courseTitle":"スマホに写真がありますよ","lessonFile":"lesson7","description":"谈论手机和照片"},
+            {"courseNumber":8,"courseTitle":"S サイズでお願いします","lessonFile":"lesson8","description":"购物时的尺寸选择"},
+            {"courseNumber":9,"courseTitle":"誕⽣⽇と性格","lessonFile":"lesson9","description":"生日和性格的表达"},
+            {"courseNumber":10,"courseTitle":"映画の約束","lessonFile":"lesson10","description":"电影约会的安排"},
+            {"courseNumber":11,"courseTitle":"電気屋さんで","lessonFile":"lesson11","description":"在电器店购物"},
+            {"courseNumber":12,"courseTitle":"漫画ファン","lessonFile":"lesson12","description":"漫画爱好者的对话"},
+            {"courseNumber":13,"courseTitle":"忘れ物","lessonFile":"lesson13","description":"遗忘物品的处理"},
+            {"courseNumber":14,"courseTitle":"勉強スケジュール","lessonFile":"lesson14","description":"学习计划的制定"},
+            {"courseNumber":15,"courseTitle":"試験勉強","lessonFile":"lesson15","description":"考试复习的准备"},
+            {"courseNumber":16,"courseTitle":"村上夏樹先生の講演会","lessonFile":"lesson16","description":"村上夏树先生的讲演会"},
+            {"courseNumber":17,"courseTitle":"アンケート","lessonFile":"lesson17","description":"问卷调查的填写"},
+            {"courseNumber":18,"courseTitle":"妹からアドバイスをもらいました","lessonFile":"lesson18","description":"从妹妹那里得到建议"},
+            {"courseNumber":19,"courseTitle":"夏休の過ごし方","lessonFile":"lesson19","description":"暑假的度过方式"},
+            {"courseNumber":20,"courseTitle":"ネットで口コミ情報をチェック","lessonFile":"lesson20","description":"网上口碑信息的查看"},
+            {"courseNumber":21,"courseTitle":"返さなくてもいいんですか","lessonFile":"lesson21","description":"不用归还也可以吗"},
+            {"courseNumber":22,"courseTitle":"チームワークが⼀番です","lessonFile":"lesson22","description":"团队合作最重要"},
+            {"courseNumber":23,"courseTitle":"グループ作業","lessonFile":"lesson23","description":"小组作业的进行"},
+            {"courseNumber":24,"courseTitle":"ランチ","lessonFile":"lesson24","description":"午餐相关的日语表达"},
+            {"courseNumber":25,"courseTitle":"日本の弁当文化","lessonFile":"lesson25","description":"日本的便当文化"}
+        ]
         },
         {
             "id": "liangs_intermediate",
@@ -99,10 +99,10 @@ module.exports = {
             ],
             "volumes": [],
             "courses": [
-              { "courseNumber": 1, "courseTitle": "第1课", "lessonFile": "lesson1", "description": "梁老师中级第1课" },
-              { "courseNumber": 2, "courseTitle": "第2课", "lessonFile": "lesson2", "description": "梁老师中级第2课" },
-              { "courseNumber": 3, "courseTitle": "第3课", "lessonFile": "lesson3", "description": "梁老师中级第3课" }
-            ]
+            {"courseNumber":1,"courseTitle":"第1课","lessonFile":"lesson1","description":"梁老师中级第1课"},
+            {"courseNumber":2,"courseTitle":"第2课","lessonFile":"lesson2","description":"梁老师中级第2课"},
+            {"courseNumber":3,"courseTitle":"第3课","lessonFile":"lesson3","description":"梁老师中级第3课"}
+        ]
         },
         {
             "id": "duolingguo",
@@ -112,17 +112,19 @@ module.exports = {
                 "duolingguo/lesson1.js",
                 "duolingguo/lesson2.js",
                 "duolingguo/lesson3.js",
+                "duolingguo/lesson4.js",
                 "duolingguo/lesson5.js"
             ],
             "volumes": [
-              { "id": "complete", "name": "完整版", "description": "多邻国完整版", "lessons": [1, 2, 3, 5] }
-            ],
+            {"id":"complete","name":"完整版","description":"多邻国完整版","lessons":[1,2,3,4,5]}
+        ],
             "courses": [
-              { "courseNumber": 1, "courseTitle": "第1课", "lessonFile": "lesson1", "description": "多邻国第1课" },
-              { "courseNumber": 2, "courseTitle": "第2课", "lessonFile": "lesson2", "description": "多邻国第2课" },
-              { "courseNumber": 3, "courseTitle": "第3课", "lessonFile": "lesson3", "description": "多邻国第3课" },
-              { "courseNumber": 5, "courseTitle": "第5课", "lessonFile": "lesson5", "description": "多邻国第5课" }
-            ]
+            {"courseNumber":1,"courseTitle":"第1课","lessonFile":"lesson1","description":"多邻国第1课"},
+            {"courseNumber":2,"courseTitle":"第2课","lessonFile":"lesson2","description":"多邻国第2课"},
+            {"courseNumber":3,"courseTitle":"第3课","lessonFile":"lesson3","description":"多邻国第3课"},
+            {"courseNumber":4,"courseTitle":"第4课","lessonFile":"lesson4","description":""},
+            {"courseNumber":5,"courseTitle":"第5课","lessonFile":"lesson5","description":"多邻国第5课"}
+        ]
         }
     ]
 };
--- scripts/update-dictionaries.js
+++ scripts/update-dictionaries.js
@@ -1,37 +1,149 @@
 const fs = require('fs');
 const path = require('path');
 
+// 定义数据库和词典文件的路径
 const databaseDir = path.join(__dirname, '../database');
-const dictionariesFile = path.join(__dirname, '../database/dictionaries.js');
+const dictionariesFile = path.join(databaseDir, 'dictionaries.js');
 
-const lessonsByDict = {};
-
-fs.readdirSync(databaseDir).forEach(dir => {
-  const dirPath = path.join(databaseDir, dir);
-  if (fs.statSync(dirPath).isDirectory()) {
-    lessonsByDict[dir] = [];
-    fs.readdirSync(dirPath).forEach(file => {
-      if (file.endsWith('.js')) {
-        const relativePath = `${dir}/${file}`;
-        lessonsByDict[dir].push(relativePath);
-      }
-    });
-  }
-});
-
-// 为了保证格式的统一和可维护性，我们直接引入并修改对象，然后重新生成文件内容
-delete require.cache[require.resolve(dictionariesFile)]; // 确保我们获取的是最新版本的文件
+// 加载现有的词典数据
+// 使用 delete require.cache 确保每次运行都加载最新文件
+delete require.cache[require.resolve(dictionariesFile)];
 const dictionariesData = require(dictionariesFile);
+const dictionaries = dictionariesData.dictionaries;
 
-// 更新每个词典的 lesson_files
-dictionariesData.dictionaries.forEach(dict => {
-  if (lessonsByDict[dict.id]) {
-    dict.lesson_files = lessonsByDict[dict.id];
-  }
+// 创建一个字典 Map 以便快速查找
+const dictMap = new Map(dictionaries.map(d => [d.id, d]));
+
+// 获取 database 目录下的所有文件夹，每个文件夹代表一个词典
+const dictDirs = fs.readdirSync(databaseDir).filter(file => {
+    const filePath = path.join(databaseDir, file);
+    // 过滤出文件夹，并排除隐藏文件夹和非文件夹（如 dictionaries.js 文件本身）
+    return fs.statSync(filePath).isDirectory() && !file.startsWith('.');
 });
 
-// 生成新的文件内容，并保持良好的格式
-const newDictionariesContent = 'module.exports = ' + JSON.stringify(dictionariesData, null, 4) + ';\n';
+// 遍历每个词典文件夹
+for (const dictId of dictDirs) {
+    const dirPath = path.join(databaseDir, dictId);
+    
+    // 查找或创建一个新的词典对象
+    let dictionary = dictMap.get(dictId);
+    if (!dictionary) {
+        console.log(`发现新词典: ${dictId}，将自动添加到 dictionaries.js`);
+        dictionary = {
+            id: dictId,
+            name: dictId, // 默认名称，可以后续手动修改
+            description: `关于 ${dictId} 的描述`, // 默认描述
+            lesson_files: [],
+            volumes: [],
+            courses: []
+        };
+        dictionaries.push(dictionary);
+        dictMap.set(dictId, dictionary);
+    }
 
-fs.writeFileSync(dictionariesFile, newDictionariesContent, 'utf8');
-console.log('dictionaries.js updated successfully.');
+    // 获取文件夹中所有的课程文件 (lesson*.js)，并按课程编号排序
+    const lessonFiles = fs.readdirSync(dirPath)
+        .filter(file => file.startsWith('lesson') && file.endsWith('.js'))
+        .sort((a, b) => {
+            const numA = parseInt(a.match(/lesson(\d+)/)[1], 10);
+            const numB = parseInt(b.match(/lesson(\d+)/)[1], 10);
+            return numA - numB;
+        })
+        .map(file => `${dictId}/${file}`);
+
+    // 1. 更新 lesson_files 数组
+    dictionary.lesson_files = lessonFiles;
+
+    // 2. 更新 courses 数组
+    const oldCourseMap = new Map((dictionary.courses || []).map(c => [c.lessonFile, c]));
+    const newCourses = [];
+
+    // 遍历课程文件，构建新的 courses 数组
+    for (const lessonPath of lessonFiles) {
+        const fileName = path.basename(lessonPath); // 例如: lesson1.js
+        const lessonFile = fileName.replace('.js', ''); // 例如: lesson1
+        
+        const existingCourse = oldCourseMap.get(lessonFile);
+
+        if (existingCourse) {
+            // 如果课程已存在，保留现有信息（保留手动修改的标题和描述）
+            newCourses.push(existingCourse);
+        } else {
+            // 如果是新课程，添加一个默认的课程对象
+            console.log(`在 ${dictId} 中发现新课程: ${fileName}，将自动添加`);
+            const match = lessonFile.match(/lesson(\d+)/);
+            const courseNumber = match ? parseInt(match[1], 10) : null;
+
+            if (courseNumber !== null) {
+                // 统一设置默认标题格式
+                const courseTitle = `第${courseNumber}课`;
+                
+                newCourses.push({
+                    courseNumber: courseNumber,
+                    courseTitle: courseTitle,
+                    lessonFile: lessonFile,
+                    description: '' // 描述留空，可手动填写
+                });
+            }
+        }
+    }
+    
+    // 按课程编号对 courses 数组进行排序
+    newCourses.sort((a, b) => a.courseNumber - b.courseNumber);
+    
+    // 替换旧的 courses 数组
+    dictionary.courses = newCourses;
+
+    // 3. 更新 volumes 中的 lessons 数组
+    // 如果词典只有一个 volume，则自动更新其 lessons 列表
+    if (dictionary.volumes && dictionary.volumes.length === 1) {
+        console.log(`正在为 ${dictId} 的单册自动更新课程列表...`);
+        const allCourseNumbers = newCourses.map(c => c.courseNumber);
+        dictionary.volumes[0].lessons = allCourseNumbers; // newCourses 已经排序，所以 allCourseNumbers 也是有序的
+    }
+}
+
+// --- 自定义格式化输出 ---
+
+// 创建一个深度克隆用于生成模板，避免修改原始数据
+const dictionariesDataForTemplate = JSON.parse(JSON.stringify(dictionariesData));
+
+// 遍历字典，用占位符替换需要特殊格式化的数组
+dictionariesDataForTemplate.dictionaries.forEach(dict => {
+    if (dict.volumes && dict.volumes.length > 0) {
+        dict.volumes = `__VOLUMES_PLACEHOLDER_${dict.id}__`;
+    }
+    if (dict.courses && dict.courses.length > 0) {
+        dict.courses = `__COURSES_PLACEHOLDER_${dict.id}__`;
+    }
+});
+
+// 生成带占位符的、整体格式化好的JSON字符串
+let newDictionariesContent = JSON.stringify(dictionariesDataForTemplate, null, 4);
+
+// 遍历原始数据，生成我们期望的紧凑数组字符串，并替换占位符
+dictionariesData.dictionaries.forEach(dict => {
+    // 格式化 volumes 数组：每个 volume 对象占一行
+    if (dict.volumes && dict.volumes.length > 0) {
+        const volumesString = `[
+${dict.volumes.map(volume => `            ${JSON.stringify(volume)}`).join(',\n')}
+        ]`;
+        newDictionariesContent = newDictionariesContent.replace(`"__VOLUMES_PLACEHOLDER_${dict.id}__"`, volumesString);
+    }
+
+    // 格式化 courses 数组：每个 course 对象占一行
+    if (dict.courses && dict.courses.length > 0) {
+        const coursesString = `[
+${dict.courses.map(course => `            ${JSON.stringify(course)}`).join(',\n')}
+        ]`;
+        newDictionariesContent = newDictionariesContent.replace(`"__COURSES_PLACEHOLDER_${dict.id}__"`, coursesString);
+    }
+});
+
+// 组合最终文件内容
+const finalContent = 'module.exports = ' + newDictionariesContent + ';\n';
+
+// 将更新后的内容写回 dictionaries.js 文件
+fs.writeFileSync(dictionariesFile, finalContent, 'utf8');
+
+console.log('dictionaries.js 已成功更新。');
